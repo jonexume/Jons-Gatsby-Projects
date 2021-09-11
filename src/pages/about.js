5f05f145d6b98b9330c8020d00f772975bfa1d79
Jon Exume <jonexume@me.com>
2021-09-10 23:28:53 -0400

    updated files
    We updated the files by adding some styling

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>I am building a great website.  You know you like it.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
